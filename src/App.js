83b5b86c4b33c89e2b20865dcbf746aa42a6d8ba
Humberto Asca <hasca@ciplima.org.pe>
2022-08-06 18:56:50 -0500

    Add: Clock running; Switching button in red and none ok. Validating react inline style.

--- src/App.js
+++ src/App.js
@@ -6,62 +6,86 @@ function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
   const [timeLeft, setTimeLeft] = useState(25);
+  const [timerOn, setTimerOn] = useState(false);
+  const [timerReset, setTimerReset] = useState(false);
 
   const incrementBreak = () => {
-    if (breakLength < 60) {
-      setBreakLength(breakLength + 1);
-    }
-    // setBreakLength(breakLength + 1);
+    !timerOn && breakLength < 60 && setBreakLength(breakLength + 1);
+    // if (breakLength < 60) {
+    //   setBreakLength(breakLength + 1);
+    // }
   }
+
   const decrementBreak = () => {
-    if (breakLength > 1) {
-      setBreakLength(breakLength - 1);
-    }
-    // setBreakLength(breakLength - 1);
+    !timerOn && breakLength > 1 && setBreakLength(breakLength - 1);
+    // if (breakLength > 1) {
+    //   setBreakLength(breakLength - 1);
+    // }
   }
   
   const incrementSession = () => {
-    if (sessionLength < 60) {
-      setSessionLength(sessionLength + 1);
-      setTimeLeft(sessionLength + 1);
-    }
-    // setSessionLength(sessionLength + 1);
-    // setTimeLeft(timeLeft + 1);
+  
+  // eslint-disable-next-line no-unused-expressions
+  !timerOn ? (
+    sessionLength < 60 ? (
+    setSessionLength(sessionLength + 1), 
+    setTimeLeft(timeLeft + 1)) : null
+    ): console.log("Aqui 2");
+    // if (sessionLength < 60) {
+    //   setSessionLength(sessionLength + 1);
+    //   setTimeLeft(sessionLength + 1);
+    // }
   }
 
   const decrementSession = () => {
-    if (sessionLength > 1) {
-      setSessionLength(sessionLength - 1);
-      setTimeLeft(timeLeft - 1);
-    }
-    // setSessionLength(sessionLength - 1);
-    // setTimeLeft(timeLeft - 1);
+    // eslint-disable-next-line no-unused-expressions
+    !timerOn ? (
+      sessionLength > 1 ? (
+      setSessionLength(sessionLength - 1), 
+      setTimeLeft(timeLeft - 1)) : null
+      ): console.log("Aqui 3");
+
+    // if (sessionLength > 1) {
+    //   setSessionLength(sessionLength - 1);
+    //   setTimeLeft(timeLeft - 1);
+    // }
   }
 
   const playStop = () => {
     myTimer ()
+    setTimerOn(!timerOn);
   }
 
   const handleMyReset = () => {
+    setTimeLeft(25);
+    setTimerOn(false);
+    setBreakLength(5);
+    setSessionLength(25);
     clearTimeout(myTimer);
-    setBreakLength(0);
-    setSessionLength(0);
-    setTimeLeft(0);
+    setTimerReset(!timerReset);
+    // setTimeLeft(25);
+    // setTimeLeft(25);
   }
 
   const  myTimer = () => {  
     setTimeout(() => {
       setTimeLeft(timeLeft + 1)
-    }, 2000);
+    }, 1000);
   }
 
   useEffect(() => {
-    // if (timeLeft === 0) {
-    //   clearTimeout(myTimer);
-    // } else {
-    //   myTimer()    
+    if (timerOn===true && timerReset === false) {
+      myTimer()
+    }
+    else if (timerOn===false && timerReset === true) {
+      setTimeLeft(25);
+    }
+
+    // else {
+    //   // clearTimeout(myTimer);
+    //   setTimeLeft(25);
     // }
-    // myTimer()    
+
   }, [timeLeft]);
 
   return (
@@ -80,7 +104,7 @@ function App() {
                 className="botones" 
                 id="break-increment"
                 onClick={() =>incrementBreak()}>🔼</button>
-              <div id="break-length">(5) {breakLength}</div>
+              <div id="break-length">{breakLength}</div> {/* aqui va 5 */}
               <button 
                 className="botones" 
                 id="break-decrement"
@@ -94,7 +118,7 @@ function App() {
                 className="botones" 
                 id="session-increment"
                 onClick={()=> incrementSession()}>🔼</button>
-              <div id="session-length">(25) {sessionLength} </div>
+              <div id="session-length">{sessionLength} </div> {/* aqui va 5 */}
               <button 
                 className="botones" 
                 id="session-decrement"
@@ -105,7 +129,10 @@ function App() {
         <div className="control-medio">
           <div className="control-medio-wrapper">
             <div id="timer-label">Session</div>
-            <div id="time-left">(25:00) {timeLeft}</div>
+            <div id="time-left">{timeLeft}:xx </div> {/* aqui va 25 */}
+            <div 
+              className="timeronoff" 
+              style={timerOn? {background:"red",borderColor:"red"}: {background:"none"}} >{timerOn ? "on": "off"} </div>
           </div>
         </div>
         <div className="control-inferior">
